5e8215689eb40fdb16abc506a1d4cfac99f30c48
zenheart <zenheart_register@163.com>
2020-06-25 17:29:50 +0800

    fix: 删除无关文件

--- examples/theory/directive/limitLength.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-Vue.directive('isEllipse', function (el, binding,vnode) {
-    vnode.context.isEllipse =el.offsetWidth < el.scrollWidth
-})
-
-
--- examples/theory/self-directive/global-init.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-Vue.directive('init',{
-    inserted:(el,binding) => {
-        el.value = binding.expression;
-    }
-})
