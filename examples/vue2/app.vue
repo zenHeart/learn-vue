7c26f49269ea56790099aa38835de7b75748473b
zenheart <zenheart_register@163.com>
2020-07-19 12:34:35 +0800

    fix: 修改导航样式

--- examples/vue2/app.vue
+++ examples/vue2/app.vue
@@ -1,37 +1,25 @@
 <template>
   <div class="app">
     <nav>
-      <div class="sort-factor">
-        <span>组织方式:</span>
-        <select v-model="selected">
-          <option v-for="option in options" v-bind:value="option.value">{{
-            option.text
-          }}</option>
-        </select>
-      </div>
+      <button @click="goHome">HOME</button>
       <a
-        :class="['nav-item', { active: id === key }]"
-        v-for="(value, key) in sortNavs"
+        :class="['nav-item', { active: currentItem.id === value.id }]"
+        v-for="value in sortNavs"
         :key="value.name"
-        :href="`#${key}`"
-        @click="id = key"
+        :href="`#${value.name}`"
       >
-        <p>
-          <span>
-            {{ value.name }}
-          </span>
-          <Tags :tags="value.tags" :tagsColor="TAGS_COLOR"></Tags>
-        </p>
+        <Tags :tags="value.tags" :tagsColor="TAGS_COLOR"></Tags>
+        <div>{{ value.name }}</div>
       </a>
     </nav>
-    <div class="app-content">
-      <component v-if="isSFC" :is="COMPONENTS[id].component"></component>
+    <div v-if="currentItem" class="app-content">
+      <component v-if="isSFC" :is="currentItem.component"></component>
       <iframe
         frameBorder="0"
         width="100%"
         height="100%"
         v-else
-        :srcdoc="COMPONENTS[id].component"
+        :srcdoc="currentItem.component"
       ></iframe>
     </div>
   </div>
@@ -40,11 +28,6 @@
 <script>
 import { COMPONENTS, TAGS_COLOR } from "./const";
 import Tags from "../common/Tags";
-const SORT_FACTOR = {
-  name: "示例名",
-  tags: "标签"
-};
-
 export default {
   name: "app",
   components: {
@@ -54,30 +37,61 @@ export default {
     return {
       COMPONENTS,
       TAGS_COLOR,
-      id: location.hash.slice(1) || 0,
-      selected: "name",
-      options: Object.keys(SORT_FACTOR).map(key => ({
-        text: SORT_FACTOR[key],
-        value: key
-      }))
+      updateTrigger: 1,
+      componentName: location.hash.slice(1),
+      tagName: new URLSearchParams(location.search).get("tag")
     };
+  },
+  methods: {
+    goHome() {
+      window.history.pushState({}, "", "/");
+      window.history.pushState({}, "", "/"); // yes twice
+      window.history.back();
+    }
   },
   computed: {
     sortNavs() {
-      let factor = this.selected;
-      return this.COMPONENTS.sort((a, b) => {
-        if (a[factor] > b[factor]) {
-          return 1;
+      if (this.tagName) {
+        return this.COMPONENTS.filter(
+          item => item.tags && item.tags.includes(this.tagName)
+        );
+      } else {
+        return this.COMPONENTS;
+      }
+    },
+    currentItem() {
+      if (this.tagName) {
+        if (this.componentName) {
+          let item = this.sortNavs.filter(
+            el => el.name === this.componentName
+          )[0];
+          if (item) {
+            return item;
+          } else {
+            // 删除 相关 hash
+            return this.sortNavs[0];
+          }
+        } else {
+          return this.sortNavs[0];
         }
-        if (a[factor] < b[factor]) {
-          return -1;
+      } else {
+        if (this.componentName) {
+          return COMPONENTS.filter(el => el.name === this.componentName)[0];
+        } else {
+          return this.sortNavs[0];
         }
-        return 0;
-      });
+      }
     },
     isSFC() {
-      return typeof this.COMPONENTS[this.id].component !== "string";
+      return this.currentItem && typeof this.currentItem.component !== "string";
     }
+  },
+  created() {
+    window.addEventListener("popstate", () => {
+      this.componentName = location.hash.slice(1);
+      this.tagName = new URLSearchParams(location.search).get("tag");
+      console.log("chage", this.componentName, this.tagName);
+    });
   }
 };
 </script>
