1d57acc19cb229930fded29d2ad76b7de62991d8
zenheart <zenheart_register@163.com>
2020-08-27 20:37:32 +0800

    docs: 补充函数组件下计算 computed 的方法

---
+++ examples/vue2/demos/FunctionComponentComputedMock/FuncOption.vue
@@ -0,0 +1,17 @@
+<template functional>
+  <p>
+    {{ $options.finishList(props.list) }}
+  </p>
+</template>
+
+<script>
+  export default {
+      funtional: true,
+      props: {
+        list: Array
+      },
+      finishList(arr) {
+        return  arr.filter(el => el.finish)
+      }
+  }
+</script>
---
+++ examples/vue2/demos/FunctionComponentComputedMock/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>模拟函数组件计算属性的方法</h2>
+    <h3>采用函数进行过滤</h3>
+    <Func :list="list"></Func>
+    <h3>采用 $options 函数进行过滤</h3>
+    <FuncOption :list="list"></FuncOption>
+  </div>
+</template>
+
+<script>
+  import Func from './Func'
+  import FuncOption from './FuncOption'
+  export default {
+    meta: {
+      tags: ['函数组件']
+    },
+    components:{
+      Func,
+      FuncOption
+    },
+    data() {
+      return {
+        list: [
+          {text: 'a',finish: false},
+          {text: 'b',finish: true},
+          {text: 'c',finish: false},
+        ]
+      }
+    }
+  }
+</script>
