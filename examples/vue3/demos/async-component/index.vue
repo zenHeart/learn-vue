6357ea1fa3b67b3541b15e107e039fb326f99da6
zenheart <zenheart_register@163.com>
2020-09-12 10:26:55 +0800

    docs: 添加 eventbus 只监听一次示例

---
+++ examples/vue3/demos/async-component/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="show^=1">load async</button>
+    <div v-if="show">
+      <Async></Async>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { defineAsyncComponent }  from 'vue'
+  
+  export default {
+    meta: {
+      tags: [ 'defineAsyncComponent' ]
+    },
+    components: {
+      Async: defineAsyncComponent(() => {
+        return new Promise((res,rej) => {
+          setTimeout(() => {
+            res(import('./Async.vue'))
+          },5000)
+        })
+      })
+    },
+    data() {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
