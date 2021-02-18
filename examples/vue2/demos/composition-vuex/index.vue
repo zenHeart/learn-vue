f6f90a519d1c488c97a61ae3e9cf484702c95441
zenheart <zenheart_register@163.com>
2021-02-18 17:56:04 +0800

    fix: 添加 sfc 导出其他模块验证

---
+++ examples/vue2/demos/composition-vuex/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <button @click="store.mutation.add">add</button>
+      <p>{{ store }}</p>
+  </div>
+</template>
+
+<script>
+import { setupStore } from './store'
+import { useSotre } from './business.js'
+
+
+  export default {
+    meta: {
+      tags: ["composition", "composition-vuex"]
+    },
+    beforeCreate() {
+      setupStore();
+    },
+    setup() {
+      return {
+        ... useSotre()
+      }
+    }
+  }
+</script>
---
+++ examples/vue2/demos/sfc-export-other/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>测试组件可以引入非 default 内容</h1>
+    <button @click="handleClick">showModal</button>
+  </div>
+</template>
+
+<script>
+  import { showModal }  from './B'
+  console.log('-----', showModal)
+  export default {
+   meta: {
+      tags: ["sfc"]
+    },
+    methods: {
+      handleClick() {
+        showModal()
+      }
+    }
+  }
+</script>
